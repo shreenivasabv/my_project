d52257b4f5e87eb57a5bf8df32d59eeffa118e3d
shreenivasbv <shreenivasbv104@gmail.com>
2025-09-10 20:06:34 +0530

    updated

--- portal/views.py
+++ portal/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def cart(request):
+    return render(request,"cart.html")
